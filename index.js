1639e622a2ec863913651bc29132cbf27d180fdb
ido <idoaviram10@gmail.com>
2023-12-23 14:23:49 +0200

    make it better!

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(cors());
 const server = http.createServer(app);
 const io = new Server(server, {
 	cors: {
-		origin: 'http://localhost:3000',
+		origin: ['http://localhost:3000', 'https://fantastic-pony-3ffa80.netlify.app'],
 	},
 });
 
